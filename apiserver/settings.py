b2b6563060ae3250b4ca2bee261cb2345d5da93b
luohu1 <mylhmail@163.com>
2021-06-17 00:51:15 +0800

    feat: use mysql database

--- apiserver/settings.py
+++ apiserver/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'apiserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'NAME': 'apiserver',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'CONN_MAX_AGE': 60
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
